187c404608a72bac6c1115fca66aeef8fd4f4a9c
shafeeka <shafeeka@users.noreply.github.com>
2026-06-21 00:17:48 -0700

    Add static config for Vercel

--- campushub/campushub/settings.py
+++ campushub/campushub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-46s6gg$bf_q&cawe%_s&j*#(noj43@y9p0@br!#43$mov0(blu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
